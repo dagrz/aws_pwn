712cca0e90495c7b2e4d2863374f728b6097a6dc
Daniel Grzelak <dgrzelak@atlassian.com>
2016-11-06 13:47:18 -0600

    Derived principals from integrations

--- elevation/dump_instance_attributes.py
+++ elevation/dump_instance_attributes.py
@@ -9,11 +9,28 @@ import os
 from datetime import datetime
 
 
+REGIONS = [
+    'ap-northeast-2',
+    'ap-northeast-1',
+    'ap-south-1',
+    'ap-southeast-1',
+    'ap-southeast-2',
+    'cn-north-1',
+    'eu-central-1',
+    'eu-west-1',
+    'sa-east-1',
+    'us-east-1',
+    'us-east-2',
+    'us-west-1',
+    'us-west-2',
+    'us-gov-west-1'
+]
+
+
 def main(args):
     if args.attributes:
-        client = boto3.client(service_name='ec2', region_name='us-east-1')
-        for region in client.describe_regions()['Regions']:
-            process_region(region['RegionName'])
+        for region in REGIONS:
+            process_region(region)
 
 
 def process_region(region_name):
